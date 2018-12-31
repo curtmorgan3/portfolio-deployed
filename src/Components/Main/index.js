e14cd6a6302b91778e06c0a73470d240c4494c48
curtmorgan3 <curtmorgan3@gmail.com>
2018-12-31 15:33:54 -0500

    Main styling

---
+++ src/Components/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './stylesheet.css';
+
+export default function Main(props){
+
+	return(
+		<div className='main'>
+			<div className='main-wrap'>
+				<h2>Full Stack Web Developer</h2>
+				<h4>Web apps from start to finish.</h4>
+				<h4>Style. Function. Scale</h4>
+			</div>
+			<a >See My Work</a>
+		</div>
+	)
+};
