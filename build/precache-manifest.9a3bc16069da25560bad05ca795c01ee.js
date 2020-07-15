00ebfe23be6d281193f178ef51e887ee77b1a4ca
Curt Morgan <curtmorgan3@gmail.com>
2020-07-15 17:23:34 -0400

    deploying

---
+++ build/precache-manifest.9a3bc16069da25560bad05ca795c01ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b057d5cf83d1c7d23531",
+    "url": "/static/css/main.6c1997dc.chunk.css"
+  },
+  {
+    "revision": "b057d5cf83d1c7d23531",
+    "url": "/static/js/main.b057d5cf.chunk.js"
+  },
+  {
+    "revision": "39185c9b651024f5a9bd",
+    "url": "/static/js/1.39185c9b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7bd4267496f81713df136b2593ee69e",
+    "url": "/static/media/wotc.f7bd4267.gif"
+  },
+  {
+    "revision": "e66f32cebf23a765206d64782cbef629",
+    "url": "/static/media/nyrr.e66f32ce.gif"
+  },
+  {
+    "revision": "1613c83d93cceda09eb48f7e5eeb66df",
+    "url": "/static/media/southwest.1613c83d.gif"
+  },
+  {
+    "revision": "711030813a0d39af02b783de9a8e89ed",
+    "url": "/static/media/higher_grounds.71103081.gif"
+  },
+  {
+    "revision": "966ead6a60c7ea8dae81c4f6914dac4a",
+    "url": "/static/media/Karma_Farm.966ead6a.gif"
+  },
+  {
+    "revision": "40a13231abde02db376ecb1b015f41ae",
+    "url": "/static/media/Table_Top.40a13231.gif"
+  },
+  {
+    "revision": "62dd47ef9a715653fa525d0c7bce00e1",
+    "url": "/static/media/Fight_or_Fright.62dd47ef.gif"
+  },
+  {
+    "revision": "85dd2b549746f01ec2e3bac6f46dde25",
+    "url": "/index.html"
+  }
+];
